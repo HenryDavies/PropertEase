8007e159a426852909c433d8885982cf22441184
HenryDavies <henrywdavies@gmail.com>
2017-04-06 12:32:43 +0100

    code refactoring - get request, parsing and scraping now all one process

--- db/seedDatabase.js
+++ db/seedDatabase.js
@@ -3,13 +3,15 @@ const parseText     = require('./2ParseText');
 const scrape        = require('./3Scrape');
 const dupesOutliers = require('./4DupesOutliers');
 
-saveProps(() => {
-  console.log('first function finished');
-  parseText(() => {
-    console.log('second function finished');
-    scrape(() => {
-      console.log('third function finished');
-      dupesOutliers();
-    });
-  });
-});
+// saveProps(() => {
+//   console.log('first function finished');
+//   parseText(() => {
+//     console.log('second function finished');
+//     scrape(() => {
+//       console.log('third function finished');
+//       dupesOutliers();
+//     });
+//   });
+// });
+
+saveProps();
