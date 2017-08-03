b9a089625c81026ed6265dfb2560be59df079254
HenryDavies <henrywdavies@gmail.com>
2017-08-03 19:22:08 +0100

    changing database seed to download last 2 days

--- db/propertySource.js
+++ db/propertySource.js
@@ -1,8 +1,9 @@
 const httpRequestPromise = require('request-promise');
-const listingsToBeSaved = 100;
+const numberOfDaysToDownload = 2;
 const listingsPerHttpRequest = 100;
 const apiKey = 'gfya4wfdf8ypa8xemktvhx6h';
 const listingsArray = [];
+const async = require('async');
 
 let options;
 
@@ -11,21 +12,31 @@ module.exports = {
 };
 
 function getAllPropertyListings(callback) {
-  const pagesToGet = (Math.ceil(listingsToBeSaved / 100) * 100) / listingsPerHttpRequest;
-  for (let pageNumber = 1; pageNumber <= pagesToGet; pageNumber++) {
-    setHttpOptions(pageNumber);
-    httpRequestPromise(options)
-    .then(data => {
-      addListingsToArray(data.listing, callback);
-      if (listingsArray.length >= listingsToBeSaved) {
-        console.log(`${listingsToBeSaved} got from Zoopla`);
-        callback(null, listingsArray.slice(0, listingsToBeSaved));
-      }
-    })
-    .catch(err => {
-      console.log('HTTP request error:', err);
-    });
-  }
+  let pageNumber = 1;
+  const xDaysAgo = Date.parse(new Date()) - 1000 * 60 * 60 * 24 * numberOfDaysToDownload;
+  let withinSpecifiedXDays = true;
+  async.whilst(
+    () => withinSpecifiedXDays && pageNumber < 6,
+    (innerCallback) => {
+      setHttpOptions(pageNumber);
+      httpRequestPromise(options)
+        .then(data => {
+          if (Date.parse(data.listing[0] < xDaysAgo)) {
+            withinSpecifiedXDays = false;
+            console.log(`${listingsArray.length} listings got from Zoopla`);
+            callback(null, listingsArray);
+          }
+          addListingsToArray(data.listing, callback);
+          console.log(`page ${pageNumber} downloaded`);
+          pageNumber++;
+          innerCallback(null, pageNumber);
+        })
+        .catch(err => {
+          console.log('HTTP request error:', err);
+        });
+    },
+    () => callback(null, listingsArray)
+  );
 }
 
 function setHttpOptions(pageNumber) {
